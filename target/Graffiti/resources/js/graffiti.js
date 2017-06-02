29cb23280da7771b112015ea9d02c8733cb1e6b3
Sara Sprenkle <sprenkles@wlu.edu>
2017-06-02 10:47:21 +0200

    Removing files that are part of the build, not the source.

--- target/Graffiti/resources/js/graffiti.js
+++
@@ -1,7 +0,0 @@
-			function hideElement(id) {
-				document.getElementById("login").style.visibility = "hidden";
-			}
-
-			function unhideElement(id) {
-				document.getElementById(id).style.visibility = "visible";
-			}
