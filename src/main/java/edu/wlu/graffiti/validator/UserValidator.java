6b125462ae8ce0bc5e5721c4dcf081f090727c6f
Sara Sprenkle <sprenkles@wlu.edu>
2019-06-14 13:51:41 -0400

    Updated formatting/whitespace

--- src/main/java/edu/wlu/graffiti/validator/UserValidator.java
+++ src/main/java/edu/wlu/graffiti/validator/UserValidator.java
@@ -4,28 +4,26 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 import edu.wlu.graffiti.bean.User;
- 
-public class UserValidator implements Validator{
- 
+
+public class UserValidator implements Validator {
+
 	@Override
 	public boolean supports(Class clazz) {
-		//just validate the user
+		// just validate the user
 		return User.class.isAssignableFrom(clazz);
 	}
- 
+
 	@Override
 	public void validate(Object target, Errors errors) {
- 
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName",
-			"required.userName", "Field name is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password",
-			"required.password", "Field name is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword",
-			"required.confirmPassword", "Field name is required.");
- 
-		User us = (User)target;
- 
-		if(!(us.getPassword().equals(us.getConfirmPassword()))){
+
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "required.userName", "Field name is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "required.password", "Field name is required.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "confirmPassword", "required.confirmPassword",
+				"Field name is required.");
+
+		User us = (User) target;
+
+		if (!(us.getPassword().equals(us.getConfirmPassword()))) {
 			errors.rejectValue("password", "notmatch.password");
 		}
 
